eba62d1a42325ffe0447d8f88e869d2aa344ba5f
Prince Kumar <princekumar5252@gmail.com>
2026-01-12 14:59:53 +0530

    feat: host release APK in public folder for provisioning

--- src/app.js
+++ src/app.js
@@ -41,6 +41,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(limiter);
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
